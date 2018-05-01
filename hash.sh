1993a428354838a053fadb21271a60fa501ddfb1
Llewellyn van der Merwe <llewellyn@vdm.io>
2018-05-01 16:25:48 +0200

    Update JCB and added info files to repo for use in shop display in JCB

--- hash.sh
+++ hash.sh
@@ -5,7 +5,7 @@ command -v jq >/dev/null 2>&1 || { echo >&2 "We require jq for this script to ru
 command -v sha1sum >/dev/null 2>&1 || { echo >&2 "We require sha1sum for this script to run, but it's not installed.  Aborting."; exit 1; }
 
 # quick checksum generator for all the Bibles used in getBible.net
-echo -n "[vdm.io] -- Building checksum for all JCB (zip) Packages found in this repository......"
+echo -n "[vdm.io] -- Building checksum and info file for all JCB (zip) Packages found in this repository......"
 
 # setup: positional arguments to pass in literal variables, query with code    
 jq_args=( )
@@ -17,9 +17,14 @@ for filename in *.zip; do
 	# get the hash
     fileHash=$(sha1sum "$filename" | awk '{print $1}')
 	# build the hash file name
-	hashFilenName="${filename/.zip/.sha}"
+	hashFileName="${filename/.zip/.sha}"
 	# create/update the file checksum
-	echo "$fileHash" > "$hashFilenName"
+	echo "$fileHash" > "$hashFileName"
+	# shop file name
+	shopFileName="${filename/.zip/.info}"
+	# pull the info file from zip for shop
+	unzip -q "$filename" info.vdm
+	mv info.vdm "$shopFileName"
 	# load the values for json
 	jq_args+=( --arg "key$nr"   "$filename"   )
     jq_args+=( --arg "value$nr" "$fileHash" )
@@ -28,7 +33,6 @@ for filename in *.zip; do
 	#next
 	nr=$((nr+1))
 done
-
 # run the generated command with jq
 jq "${jq_args[@]}" "$jq_query" <<<'{}' > checksum.json
 
